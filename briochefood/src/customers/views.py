163ab2339b3c2c809adb03f88109b8ef303c9bed
Dianna-coder <diana270301@hotmail.com>
2021-04-29 14:51:44 -0300

    teste

--- briochefood/src/customers/views.py
+++ briochefood/src/customers/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from customers.models import Customer
 from customers.serializer import CustomerSerializer
 
-
+# adicionar comentário
 class CustomerView(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
